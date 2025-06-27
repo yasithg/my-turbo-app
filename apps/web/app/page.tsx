46aa4b926d469f2952c574fecc6f446953164ba2
Yasith Gamage <dev.yasith@Yasiths-MacBook-Air.local>
2025-06-27 17:20:08 +0530

    change layout

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -44,6 +44,9 @@ export default function Home() {
 
         <div className={styles.ctas}>
           <TodoList />
+        </div>
+        <div className={styles.ctas}>
+          <br/>
           <a
             className={styles.primary}
             href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
